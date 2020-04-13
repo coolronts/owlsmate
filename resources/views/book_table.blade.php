dc46b1e43096387c9cf4d05803304f32f5ea78f6
coolronts <58170502+coolronts@users.noreply.github.com>
2020-04-13 05:12:11 +0200

    text align

--- resources/views/book_table.blade.php
+++ resources/views/book_table.blade.php
@@ -157,7 +157,7 @@
                               </h5>
                               
                               <div class="card-description" style="margin-top:-20px; text-align: left;">
-                                <p style="margin:5px">Course Name: {{$row_book->course_name }}</p>
+                                <p style="text-align:left;margin:5px">Course Name: {{$row_book->course_name }}</p>
                                 
                                <p style="margin-top:-10px"> Course ID: {{$row_book->course_id}}</p>
                                 
