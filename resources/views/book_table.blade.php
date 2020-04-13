9da30884de5ba6459d3ace68837934a33f546cd4
coolronts <58170502+coolronts@users.noreply.github.com>
2020-04-13 14:14:56 +0200

    sad

--- resources/views/book_table.blade.php
+++ resources/views/book_table.blade.php
@@ -156,7 +156,7 @@
                                 <small style="font-size:12px">written by</small> <strong>{{$row_book->author}}</strong>
                               </h5>
                               
-                              <div class="card-description" style="margin-top:-10px" >
+                              <div class="card-description" style="margin-top:-30px" >
                                 <small style="font-size:12px;font-weight:lighter;margin-top:"><u><b>Course Name</b></u></small>
                                 <h6 style="margin-top:-10px;" >
                                   <strong>{{$row_book->course_name}}</strong>
