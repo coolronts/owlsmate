1fb58d3627cf0ad21e6f0137c08ae33959f89de5
coolronts <58170502+coolronts@users.noreply.github.com>
2020-04-13 14:20:54 +0200

    style

--- resources/views/book_table.blade.php
+++ resources/views/book_table.blade.php
@@ -161,7 +161,7 @@
                                 <h6 style="margin-top:-10px;" >
                                   <strong>{{$row_book->course_name}}</strong>
                                 </h6>
-                               <div class="marign-top:-20px">
+                               <div style="marign-top:-20px">
                                 <small style="font-size:12px;font-weight:lighter;margin-top:-200px"><u><b>Course ID</b></u></small>
                                 <h6 style="margin-top:-10px" >
                                   <strong>{{$row_book->course_id}}</strong>
