30ce559ce30c4ffb5c331bc727ba162710b35d17
coolronts <58170502+coolronts@users.noreply.github.com>
2020-04-13 05:32:41 +0200

    change

--- resources/views/book_table.blade.php
+++ resources/views/book_table.blade.php
@@ -157,7 +157,10 @@
                               </h5>
                               <br>
                               <p class="card-description" style="margin-top:-20px; text-align: left;font-weight:lighter; line-height: 2em;width: 300px; overflow: hidden;position: relative; ">
-                               <div class="row"> <label class="col-md-5">Course Name:</label> {{$row_book->course_name }}
+                               <div class="row"> 
+                                 <label class="col-md-5">Course Name:</label>
+                                 <div class="col-md-7" > {{$row_book->course_name }}
+                                 </div>
                                </div>
                                 <br>
                                 Course ID: {{$row_book->course_id}}
