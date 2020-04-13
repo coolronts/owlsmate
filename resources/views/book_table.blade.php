ff3305b1e6da1ab5ea6c1978204097ecbc1d006a
coolronts <58170502+coolronts@users.noreply.github.com>
2020-04-13 13:33:07 +0200

    1em

--- resources/views/book_table.blade.php
+++ resources/views/book_table.blade.php
@@ -157,7 +157,7 @@
                               </h5>
                               <br>
                               <div class="card-description" >
-                              <p style="margin-top:-20px; text-align: left;font-weight:lighter; line-height: 2em;width: 300px; overflow: hidden;position: relative; height: 16em;">
+                              <p style="margin-top:-20px; text-align: left;font-weight:lighter; line-height: 2em;width: 300px; overflow: hidden;position: relative; height: 1em;">
                                 Course Name: {{$row_book->course_name }}</p>
                                 
                                <p> Course ID: {{$row_book->course_id}}</p>
